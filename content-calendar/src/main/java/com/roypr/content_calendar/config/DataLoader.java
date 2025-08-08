a5cb980521a2a76a93ea43aaf2b7e3b13f34e96f
proy05 <79052853+proy05@users.noreply.github.com>
2025-08-09 01:09:45 +0530

    Using config.DataLoader(CommandLineRunner) and jackson.databind.ObjectMapper to deserialize content.json in resources/data folder into List<Content> to save sample rows into ContentRepository H2 db

--- content-calendar/src/main/java/com/roypr/content_calendar/config/DataLoader.java
+++ content-calendar/src/main/java/com/roypr/content_calendar/config/DataLoader.java
@@ -1,16 +1,46 @@
 package com.roypr.content_calendar.config;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.roypr.content_calendar.model.Content;
+import com.roypr.content_calendar.repository.ContentRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-@Profile("!dev") //Don't run below bean(DataLoader) when active profile is dev, meaning dev env.
+import java.io.InputStream;
+import java.util.List;
+
+//@Profile("!dev") //Don't run below bean(DataLoader) when active profile is dev, meaning dev env.
 @Component
 public class DataLoader implements CommandLineRunner {
     //CommandLineRunner can be used to run some bootstrapping code like db row loading.
     //It is run after dependency injection is done as the app starts up.
+
+    private final ContentRepository repository;
+    private final ObjectMapper objectMapper;
+
+    public DataLoader(ContentRepository repository, ObjectMapper objectMapper) {
+        this.repository = repository;
+        this.objectMapper = objectMapper;
+    }
+
+
+    /**
+    Loads a JSON file from resources.
+    Deserializes it into a List<Content>.
+    Saves all items to the repository.
+    Closes the file automatically.
+     */
     @Override
     public void run(String... args) throws Exception {
         System.out.println("Hello from DataLoader :)");
+        try(InputStream inputStream = TypeReference.class.getResourceAsStream("/data/content.json")){
+            repository.saveAll(objectMapper.readValue(
+                    inputStream,
+                    new TypeReference<List<Content>>(){} //this argument is an anonymous inner class implementing abstract TypeReference<T>
+                    )
+            );
+        }
     }
 }
